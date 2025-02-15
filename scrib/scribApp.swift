c141c8c34abf0fb36cb9a35a0a9c67f893948083
sourav-bz <s.kumar001997@gmail.com>
2025-02-16 00:15:02 +0530

    Initial Commit

---
+++ scrib/scribApp.swift
@@ -0,0 +1,17 @@
+//
+//  scribApp.swift
+//  scrib
+//
+//  Created by Sourav on 16/02/25.
+//
+
+import SwiftUI
+
+@main
+struct scribApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ scribTests/scribTests.swift
@@ -0,0 +1,17 @@
+//
+//  scribTests.swift
+//  scribTests
+//
+//  Created by Sourav on 16/02/25.
+//
+
+import Testing
+@testable import scrib
+
+struct scribTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
